efa56bfba8d04a5187b38260d74754444a233564
matttalda <taldam1@udayton.edu>
2019-11-12 10:20:32 -0500

    ssh_nethack4 can now logon to the AWS server and recieve the correct json response in return

--- protoypes/ssh_nethack4.py
+++ protoypes/ssh_nethack4.py
@@ -2,21 +2,41 @@ import json
 import sys
 import paramiko
 import re
+import time
 
 # Logon Info
-hostname = 'nethack4.org'
-username = 'nethack43'
-password = 'nethack43'
+hostname = 'ec2-34-229-53-61.compute-1.amazonaws.com'
+username = 'ubuntu'
+password = ''
+PKey = 'NetCrawler.pem'
 
 # Connect via SSH
-client = paramiko.SSHClient()
-client.load_system_host_keys()
-client.connect(hostname, port=22, username=username, password=password)
-chnl = client.invoke_shell(width=100,height=100)
+def connectSSH(hostname, username, password, keyFile):
+   try:
+      key = paramiko.RSAKey.from_private_key_file(keyFile)
+      client = paramiko.SSHClient()
+      client.load_system_host_keys()
+      client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+      client.connect(hostname, port=22, username=username, password=password, pkey=key)
+      print("LOG: Connected!", flush=True)
+      return client
+   except Exception as e:
+      print("LOG: ", e, flush=True)
+      print("LOG: Exiting...", flush=True)
+      quit() 
 
-# Create I/O
-stdin = chnl.makefile('w')
-stdout = chnl.makefile('r')
+def readSSH(stdout, stderr):
+   start = time.time()
+   print("LOG: Reading...", flush=True)
+   time.sleep(5)
+   while stdout.channel.recv_ready() == True:
+      line = stdout.read(1)
+      print(ansi_escape.sub('', line.decode('utf-8')), end='')
+   end = time.time()
+   timeTaken = end - start
+   print("\nTime Elapsed Reading: ", timeTaken, flush=True)
+
+client = connectSSH(hostname, username, password, PKey)
 
 # Removes ANSI chars
 ansi_escape = re.compile(r'''
@@ -27,15 +47,31 @@ ansi_escape = re.compile(r'''
     [@-~]   # Final byte
 ''', re.VERBOSE)
 
+# Start NetHack4
+print("LOG: Writing...", flush=True)
+stdin, stdout, stderr = client.exec_command('./startServer')
 
-for line in stdout.read().splitlines():
-   print(ansi_escape.sub('', line.decode("utf-8")))
-
-
+# Auth
+auth = {"auth": {"username": "xxxx", "password": "yyyy", "email": "NULL"}}
+data = json.dumps(auth)
+data.encode('utf-8')
+stdin.write(data)
 stdin.write('\n')
-stdin.flush()
 
-for line in stdout.read().splitlines():
-   print(ansi_escape.sub('', line.decode("utf-8")))
+readSSH(stdout, stderr)
 
+# Create a game
+#json_message = {"get_options": {"options": ["gamemode", "character", "character name"]}}
+#data = json.dumps(json_message)
+#data.encode('utf-8')
+#stdin.write(data)
+#stdin.write('\n')
+
+#readSSH(stdout, stderr)
+
+# Close client
+stdin.close()
+stdout.close()
+stderr.close()
 client.close()
+
